858f2deaab9471321cada9720395f0d4a530ad04
Armaan Sandhu <armaansndhu@gmail.com>
2021-08-23 01:18:13 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo">
+          <img src="https://logodix.com/logo/1851783.png" alt="todolist" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
